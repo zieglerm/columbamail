ddc31dd74ae78989f7cbd4f3fa2f5eea710a0537
hubms <hubms@410de10b-2318-0410-a529-9c54226a8375>
2006-09-26 23:59:29 +0000

    made the tagging command virtual folder ready

--- columba/trunk/mail/src/main/java/org/columba/mail/folder/command/TagMessageCommand.java
+++ columba/trunk/mail/src/main/java/org/columba/mail/folder/command/TagMessageCommand.java
@@ -2,18 +2,28 @@ package org.columba.mail.folder.command;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Hashtable;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
 
 import org.columba.api.command.IWorkerStatusController;
 import org.columba.core.association.AssociationStore;
 import org.columba.core.command.Command;
+import org.columba.core.folder.api.IFolder;
 import org.columba.mail.command.IMailFolderCommandReference;
 import org.columba.mail.folder.IMailbox;
+import org.columba.mail.folder.virtual.VirtualFolder;
+import org.columba.mail.folder.virtual.VirtualHeader;
+import org.columba.mail.message.IHeaderList;
 
 public class TagMessageCommand extends Command {
 
 	public final static int ADD_TAG = 0;
 
 	public final static int REMOVE_TAG = 1;
+
+	private static final Logger LOG = Logger
+	.getLogger("org.columba.mail.folder.command.TagMessageCommand");
 
 	private int type;
 
@@ -37,15 +47,40 @@ public class TagMessageCommand extends Command {
 		// get source folder
 		IMailbox srcFolder = (IMailbox) r.getSourceFolder();
 
+		Hashtable<Object, IFolder> mails = new Hashtable<Object, IFolder>();
+
+		// check if virtual folder, if yes, do not use these uids, use the
+		// real uids instead
+		if (srcFolder instanceof VirtualFolder) {
+			// get original folder
+			try {
+
+				IHeaderList hl = ((IMailbox) r.getSourceFolder())
+						.getHeaderList();
+				for (Object uid : r.getUids()) {
+					// should be virtual
+					mails.put(((VirtualHeader) hl.get(uid)).getSrcUid(),
+							((VirtualHeader) hl.get(uid)).getSrcFolder());
+				}
+			} catch (Exception e) {
+				LOG.severe("Error getting header list from virtual folder");
+				e.printStackTrace();
+			}
+		} else {
+			for (Object uid : r.getUids()) {
+				mails.put(uid, r.getSourceFolder());
+			}
+		}
+
 		if (type == ADD_TAG) {
-			for (Object currentItem : r.getUids()) {
+			for (Entry<Object, IFolder> entry : mails.entrySet()) {
 				AssociationStore.getInstance().addAssociation("tagging", id,
-						createURI(srcFolder.getId(), currentItem).toString());
+						createURI(entry.getValue().getId(), entry.getKey()).toString());
 			}
 		} else if (type == REMOVE_TAG) {
-			for (Object currentItem : r.getUids()) {
+			for (Entry<Object, IFolder> entry : mails.entrySet()) {
 				AssociationStore.getInstance().removeAssociation("tagging", id,
-						createURI(srcFolder.getId(), currentItem).toString());
+						createURI(entry.getValue().getId(), entry.getKey()).toString());
 			}
 		}
 	}
