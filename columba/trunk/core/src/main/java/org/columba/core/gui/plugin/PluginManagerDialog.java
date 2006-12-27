720556fac51306763026123ffe2957fe5fc8a419
fdietz <fdietz@410de10b-2318-0410-a529-9c54226a8375>
2006-12-27 10:13:30 +0000

    [bug] #CA-167 Plugin Manager bug when adding/removing plugins

--- columba/trunk/core/src/main/java/org/columba/core/gui/plugin/PluginManagerDialog.java
+++ columba/trunk/core/src/main/java/org/columba/core/gui/plugin/PluginManagerDialog.java
@@ -1,16 +1,16 @@
 //The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
 //The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 
@@ -66,9 +66,9 @@ import org.columba.core.resourceloader.GlobalResourceLoader;
 
 /**
  * @author fdietz
- * 
+ *
  * This dialog lets you view all installed plugins in a categorized tree view.
- * 
+ *
  * There are buttons which let you: - install new plugins - remove plugins -
  * enable/disable plugins - view plugin info (readme.txt in plugin folder)
  */
@@ -78,7 +78,7 @@ public class PluginManagerDialog extends JDialog implements ActionListener,
 
 	private static final Logger LOG = Logger
 	.getLogger("org.columba.core.gui.plugin");
-	
+
 	protected JButton installButton;
 
 	protected JButton removeButton;
@@ -97,7 +97,9 @@ public class PluginManagerDialog extends JDialog implements ActionListener,
 
 	protected PluginNode selectedNode;
 
-	public PluginManagerDialog() {
+	private static PluginManagerDialog instance;
+
+	private PluginManagerDialog() {
 		// modal JDialog
 		super((JFrame) null, GlobalResourceLoader.getString(RESOURCE_PATH,
 				"pluginmanager", "title"), true);
@@ -113,6 +115,14 @@ public class PluginManagerDialog extends JDialog implements ActionListener,
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
+	}
+
+	public static PluginManagerDialog getInstance() {
+		if ( instance == null) {
+			instance = new PluginManagerDialog();
+		}
+
+		return instance;
 	}
 
 	protected void initComponents() {
@@ -376,7 +386,7 @@ public class PluginManagerDialog extends JDialog implements ActionListener,
 				"plugins");
 
 		LOG.info("extract "+file.getName()+" to "+ destination.getAbsolutePath());
-		
+
 		File pluginDirectory;
 		try {
 			// extract plugin
@@ -396,17 +406,17 @@ public class PluginManagerDialog extends JDialog implements ActionListener,
 			// the plugin directory is "<config-folder>/plugins/<plugin-id>"
 			pluginDirectory = new File(destination, pluginDirectory.getName());
 			LOG.info("plugin directory="+pluginDirectory.getAbsolutePath());
-			
+
 			// the path to the plugin.xml descriptor file is:
 			// "<config-folder>/plugins/<plugin-id>/plugin.xml
 			pluginDirectory = new File(pluginDirectory, "plugin.xml");
-			
+
 			String id = PluginManager.getInstance().addPlugin(pluginDirectory);
 			PluginMetadata metadata = PluginManager.getInstance()
 					.getPluginMetadata(id);
 
 			table.addPlugin(metadata);
-			
+
 			JOptionPane.showMessageDialog(this, GlobalResourceLoader.getString(
 					RESOURCE_PATH, "pluginmanager", "installSuccess.msg"),
 					GlobalResourceLoader.getString(RESOURCE_PATH,
