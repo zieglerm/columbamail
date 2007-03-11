da9c65f4e166d0f80576dea56fa7ff10e9469815
fdietz <fdietz@410de10b-2318-0410-a529-9c54226a8375>
2007-03-11 13:36:55 +0000

    [bug]fixed performance bug in menu painting

--- columba/trunk/mail/src/main/java/org/columba/mail/gui/tree/util/FolderTreeCellRenderer.java
+++ columba/trunk/mail/src/main/java/org/columba/mail/gui/tree/util/FolderTreeCellRenderer.java
@@ -1,16 +1,16 @@
 //The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
 //The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 
@@ -55,19 +55,18 @@ public class FolderTreeCellRenderer extends DefaultTreeCellRenderer {
 	private static Icon trashIcon;
 
 	private static Icon templateIcon;
+
 	private static Icon sentIcon;
 
 	static {
-		collapsedFolderIcon = ImageLoader
-				.getSmallIcon("folder.png");
+		collapsedFolderIcon = ImageLoader.getSmallIcon("folder.png");
 		expandedFolderIcon = ImageLoader.getSmallIcon("folder-open.png");
 		virtualFolderIcon = ImageLoader.getSmallIcon("folder-saved-search.png");
 		localRootFolderIcon = ImageLoader.getSmallIcon("computer.png");
-		imapRootFolderIcon = ImageLoader
-				.getSmallIcon("network-server.png");
+		imapRootFolderIcon = ImageLoader.getSmallIcon("network-server.png");
 		inboxIcon = MailImageLoader.getSmallIcon("folder-inbox-new.png");
 		outboxIcon = MailImageLoader.getSmallIcon("folder-outbox.png");
-		
+
 		templateIcon = MailImageLoader.getSmallIcon("folder-template.png");
 		sentIcon = MailImageLoader.getSmallIcon("folder-sent.png");
 		trashIcon = ImageLoader.getSmallIcon("user-trash.png");
@@ -113,12 +112,12 @@ public class FolderTreeCellRenderer extends DefaultTreeCellRenderer {
 				leaf, row, hasFocusVar);
 
 		// setting default Values
-		setFont(plainFont);
-		setToolTipText("");
+		// setFont(plainFont);
+		// setToolTipText("");
 
 		IMailFolder treeNode = (IMailFolder) value;
-		setText(treeNode.getName());
-		setIcon(getFolderIcon(treeNode, expanded));
+		// setText(treeNode.getName());
+		// setIcon(getFolderIcon(treeNode, expanded));
 
 		if (value instanceof IMailbox) {
 			IMailbox folder = (IMailbox) value;
@@ -138,13 +137,17 @@ public class FolderTreeCellRenderer extends DefaultTreeCellRenderer {
 			// set label text
 			setText(createLabelText(info, item));
 
+		} else {
+			setText(treeNode.getName());
 		}
+
+		setIcon(getFolderIcon(treeNode, expanded));
 		return this;
 	}
 
 	/**
 	 * Create HTML label text
-	 * 
+	 *
 	 * @param info
 	 *            mailbox info (total/unread/recent count)
 	 * @param item
@@ -159,48 +162,53 @@ public class FolderTreeCellRenderer extends DefaultTreeCellRenderer {
 		// - Is this folder selectable (does it contain messages?)
 		boolean not_selectable = item.getBooleanWithDefault("selectable", true) == false;
 
-		StringBuffer buf;
-		// set label text
-		buf = new StringBuffer("<html><body>");
+		if (not_selectable || info.getUnseen() > 0 || info.getRecent() > 0) {
 
-		// if IMAP selectable folder
-		// -> lightgray font color and italic
-		if (not_selectable)
-			buf.append("<i><font color='#333333'>");
+			StringBuffer buf;
+			// set label text
+			buf = new StringBuffer("<html><body>");
 
-		if (info.getUnseen() > 0) {
-			// draw folder name and unseen count
-			// -> using bold font
-			buf.append("<b>");
+			// if IMAP selectable folder
+			// -> lightgray font color and italic
+			if (not_selectable)
+				buf.append("<i><font color='#333333'>");
 
-			// append unseen count to folder name
-			name = name + " (" + info.getUnseen() + ")";
-			buf.append(name);
+			if (info.getUnseen() > 0) {
+				// draw folder name and unseen count
+				// -> using bold font
+				buf.append("<b>");
 
-			buf.append("</b>");
-		} else
-			buf.append(name);
+				// append unseen count to folder name
+				name = name + " (" + info.getUnseen() + ")";
+				buf.append(name);
 
-		// if IMAP selectable folder
-		if (not_selectable)
-			buf.append("</i>");
+				buf.append("</b>");
+			} else
+				buf.append(name);
 
-		if (info.getRecent() > 0) {
-			// append recent message count
-			// -> using blue font
-			buf.append("<font color='blue'> (");
-			buf.append(info.getRecent());
-			buf.append(")");
+			// if IMAP selectable folder
+			if (not_selectable)
+				buf.append("</i>");
+
+			if (info.getRecent() > 0) {
+				// append recent message count
+				// -> using blue font
+				buf.append("<font color='blue'> (");
+				buf.append(info.getRecent());
+				buf.append(")");
+			}
+
+			buf.append("</body></html>");
+
+			return buf.toString();
+		} else {
+			return name;
 		}
-
-		buf.append("</body></html>");
-
-		return buf.toString();
 	}
 
 	/**
 	 * Create HTML tooltip text.
-	 * 
+	 *
 	 * @param info
 	 *            mailboxinfo (total/unread/recent count)
 	 * @return tooltip text
@@ -229,13 +237,13 @@ public class FolderTreeCellRenderer extends DefaultTreeCellRenderer {
 			if (folder.isInboxFolder()) {
 				return inboxIcon;
 			} else if (folder.getId().equals("103")) {
-				
+
 				return outboxIcon;
 			} else if (folder.getId().equals("107")) {
 				return templateIcon;
-			}else if (folder.getId().equals("104")) {
+			} else if (folder.getId().equals("104")) {
 				return sentIcon;
-			}else if (folder.isTrashFolder()) {
+			} else if (folder.isTrashFolder()) {
 				return trashIcon;
 			}
 		}
